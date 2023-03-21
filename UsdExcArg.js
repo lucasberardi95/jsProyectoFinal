64a33bd4a043796f882fa4dd6aaccff9a22c5210
Lucas Berardi <lucas-l-@live.com>
2023-03-21 04:06:55 -0300

    cambios grales, librerias, fetch, etc.

--- UsdExcArg.js
+++ UsdExcArg.js
@@ -1,26 +1,14 @@
-class tipoDeDolar {
-    constructor(id, nombre, precio, img){
-        this.id = id;
-        this.nombre = nombre; 
-        this.precio = precio;
-        this.img = img; 
-        this.cantidad = 1;
-    }
-}
-
-const one = new tipoDeDolar(1, "One Dollar", 377, "./img/oneDollar.jpg");
-const two = new tipoDeDolar(2, "Two Dollars", 754, "./img/twoDollars.jpg");
-const five = new tipoDeDolar(3, "Five Dollars", 1885, "./img/fiveDollars.jpg");
-const ten = new tipoDeDolar(4, "Ten Dollars", 3770, "./img/tenDollars.jpg");
-const twenty = new tipoDeDolar(5, "Twenty Dollars", 7540, "./img/twentyDollars.jpg");
-const fifty = new tipoDeDolar(6, "Fifty Dollars", 18850, "./img/fiftyDollars.jpg");
-const oneHundred = new tipoDeDolar(7, "One Hundred Dollars", 37700, "./img/oneHundredDollars.jpg");
-
-
-const arrayTiposDeDolar = [one, two, five, ten, twenty, fifty, oneHundred];
-console.log(arrayTiposDeDolar);
-
+let dolares = [];
 let carroDeCompras = [];
+const miUrl = "json/productos.json"
+
+fetch(miUrl)
+    .then(response => response.json())
+    .then(data =>{
+        dolares = data;
+        mostrarDolares(data);
+    })
+    .catch(error => console.log(error));
 
 if(localStorage.getItem("carroDeCompras")){
     carroDeCompras = JSON.parse(localStorage.getItem("carroDeCompras"));
@@ -29,40 +17,47 @@ if(localStorage.getItem("carroDeCompras")){
 const contenedorDolares = document.getElementById("contenedorDolares");
 
 const mostrarDolares = () => {
-    arrayTiposDeDolar.forEach(tipoDeDolar => {
+    dolares.forEach(dolar => {
         const card = document.createElement("div");
         card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
         card.innerHTML = `
                         <div class = "card">
-                            <img src = "${tipoDeDolar.img}" class = "card-img-top imgTipoDeDolar" alt = "${tipoDeDolar.nombre}">
+                            <img src = "${dolar.img}" class = "card-img-top imgTipoDeDolar" alt = "${dolar.nombre}">
                             <div>
-                                <h3>${tipoDeDolar.nombre}</h2>
-                                <p>${tipoDeDolar.precio}</p>
-                                <button class = "btn colorBoton " id = "boton${tipoDeDolar.id}">Agregar al carro</button>
+                                <h3>${dolar.nombre}</h2>
+                                <p>${dolar.precio}</p>
+                                <button class = "btn colorBoton " id = "boton${dolar.id}">Agregar al carro</button>
                             </div>
                         </div>
                         `
         contenedorDolares.appendChild(card)  
         
-        const boton = document.getElementById(`boton${tipoDeDolar.id}`);
+        const boton = document.getElementById(`boton${dolar.id}`);
         boton.addEventListener("click", () => {
-        agregarAlCarro(tipoDeDolar.id);
+            agregarAlCarro(dolar.id);
+            Toastify({
+                text: "Verde agregado al carro",
+                duration: 2000,
+                close: true,
+                gravity: "top",
+                position: "right",
+                backgroundColor: "darkgreen",
+            }).showToast();
         })
     })
 }
 
-mostrarDolares();
-
 const agregarAlCarro = (id) => {
-    const dolarEnCarro = carroDeCompras.find(tipoDeDolar => tipoDeDolar.id === id);
+    const dolar = dolares.find(dolar => dolar.id === id);
+    const dolarEnCarro = carroDeCompras.find(dolar => dolar.id === id);
     if(dolarEnCarro) {
         dolarEnCarro.cantidad++;
     } else {
-        const tipoDeDolar = arrayTiposDeDolar.find(tipoDeDolar => tipoDeDolar.id === id);
-        carroDeCompras.push(tipoDeDolar);
+        carroDeCompras.push(dolar);
     }
     calcularTotal();
     localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+    mostrarCarro();
 }
 
 const contenedorCarro = document.getElementById("contenedorCarro");
@@ -74,33 +69,64 @@ verCarro.addEventListener("click", () => {
 
 const mostrarCarro = () => {
     contenedorCarro.innerHTML = "";
-    carroDeCompras.forEach(tipoDeDolar => {
+    carroDeCompras.forEach(dolar => {
         const card = document.createElement("div");
         card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
         card.innerHTML = `
                         <div class = "card">
-                            <img src = "${tipoDeDolar.img}" class = "card-img-top imgTipoDeDolar" alt = "${tipoDeDolar.nombre}">
+                            <img src = "${dolar.img}" class = "card-img-top imgTipoDeDolar" alt = "${dolar.nombre}">
                             <div>
-                                <h3>${tipoDeDolar.nombre}</h2>
-                                <p>${tipoDeDolar.precio}</p>
-                                <p>${tipoDeDolar.cantidad}</p>
-                                <button class = "btn colorBoton " id = "eliminar${tipoDeDolar.id}">Sacar del carro</button>
+                                <h3>${dolar.nombre}</h2>
+                                <p>${dolar.precio}</p>
+                                <p>${dolar.cantidad}</p>
+                                <button class = "btn colorBoton " id = "eliminar${dolar.id}">Sacar del carro</button>
+                                <button class = "btn colorBoton " id = "aumentar${dolar.id}"> + </button>
+                                <button class = "btn colorBoton " id = "disminuir${dolar.id}"> - </button>
                             </div>
                         </div>
                         `
         contenedorCarro.appendChild(card);
 
-        const boton = document.getElementById(`eliminar${tipoDeDolar.id}`);
+        const disminuirUno = document.getElementById(`disminuir${dolar.id}`);
+        disminuirUno.addEventListener("click", () => {
+        restarUno(dolar.id);
+        });
+
+        const agregarUno = document.getElementById(`aumentar${dolar.id}`);
+        agregarUno.addEventListener("click", () => {
+        sumarUno(dolar.id);
+        });
+        
+
+        const boton = document.getElementById(`eliminar${dolar.id}`);
         boton.addEventListener("click", () => {
-        SacarDelCarro(tipoDeDolar.id);
+        SacarDelCarro(dolar.id);
         })
     })
     calcularTotal();
 }
 
+const sumarUno = (id) => {
+    const dolar = carroDeCompras.find(dolar => dolar.id === id);
+    dolar.cantidad++;
+    localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+    mostrarCarro();
+}
+
+const restarUno = (id) => {
+    const dolar = carroDeCompras.find(dolar => dolar.id === id);
+    dolar.cantidad--;
+    if(dolar.cantidad === 0){
+        SacarDelCarro(id);
+    }else{
+        localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras))
+    }
+    mostrarCarro();
+}
+
 const SacarDelCarro = (id) => {
-    const tipoDeDolar = carroDeCompras.find(tipoDeDolar => tipoDeDolar.id === id);
-    const indice = carroDeCompras.indexOf(tipoDeDolar);
+    const dolar = carroDeCompras.find(dolar => dolar.id === id);
+    const indice = carroDeCompras.indexOf(dolar);
     carroDeCompras.splice(indice, 1);
     mostrarCarro();
     localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
@@ -127,3 +153,14 @@ const eliminarTodoElCarro = () => {
     mostrarCarro();
     localStorage.clear();
 }
+
+const comprar = document.getElementById("comprar");
+
+comprar.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Seguro que deseas llevarte los verdes ya mismo?',
+        text: 'Te recomendaria que si, cada segundo que pasa tu moneda vale menos que un samuelin!',
+        icon: 'question',
+        confirmButtonText: 'SIII'
+    })
+})
